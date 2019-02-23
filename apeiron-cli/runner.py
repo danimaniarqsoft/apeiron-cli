829a2d8d11ab24798647260a1656a9c64f1be3a4
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-02-22 18:20:30 -0600

    adding

--- apeiron-cli/runner.py
+++ apeiron-cli/runner.py
@@ -1,5 +1,6 @@
+
 import sys
-sys.path.append('C:\\Users\\daniel.cortes\\development\\git\\apeiron-cli\\apeiron-cli\\calculation')
+sys.path.append('C:\\Users\\daniel.cortes\\development\\git\\apeiron-cli\\apeiron-cli\\reader')
 
 import reader
 print(reader.add(2,3))
