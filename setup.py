c6a159b0a1164866fc99e4f6abc7c60b14c2cbee
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-02-26 23:28:54 -0600

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='apeiron-cli',
+      version='0.0.1',
+      description='The apeiron cli',
+      url='https://github.com/danimaniarqsoft/apeiron-cli',
+      author='Daniel Cortes Pichardo',
+      license='MIT',
+      packages=['apeiron-cli'],
+      zip_safe=False)
